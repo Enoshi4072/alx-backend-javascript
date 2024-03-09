509530a3d3bcf53d3457fcfebf382800e540d353
Enoshi4072 <enockkibet6642@gmail.com>
2024-03-09 13:15:50 +0000

    JS classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,16 @@
 export default class Building {
 	constructor (sqft) {
-		if (new.target === Building) {
-			throw new Error('Cannot instantiate abstract class Building');
+		this._sqft = sqft; 
+		if (this.constructor !== Building ){
+			if (this.evacuationWarningMessage == undefined) {
+				throw new Error ('Class extending Building must override evacuationWarningMessage');
+			}
 		}
-		this._sqft = sqft;
 	}
 	get sqft() {
 		return this._sqft;
 	}
-	evacuationWarningMessage () {
-		throw new Error('Class extending Building must override evacuationWaringMessage');
+	set sqft(value) {
+		this._sqft = value;
 	}
 }
