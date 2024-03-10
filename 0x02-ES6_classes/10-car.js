042ea4938557fca54aa2bd181f482df20bc82a72
Enoshi4072 <enockkibet6642@gmail.com>
2024-03-10 08:40:03 +0000

    10. Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,43 +1,49 @@
 export default class Car {
-	constructor (brand, motor, color) {
-		this._brand = brand;
-		this._motor = motor;
-		this._color = color;
-	}
-	get brand() {
-		return this._brand;
-	}
-	set brand(value) {
-		if (typeof value !== 'string') {
-			throw new Error ('Value must be a string');
-		}
-		this._brand = value;
-	}
-	get motor() {
-                return this._motor;
-        }
-        set motor(value) {
-                if (typeof value !== 'string') {
-                        throw new Error ('Value must be a string');
-                }
-                this._motor = value;
-        }
-	get color() {
-                return this._color;
-        }
-        set color(value) {
-                if (typeof value !== 'string') {
-                        throw new Error ('Value must be a string');
-                }
-                this._color = value;
-        }
-	static get [Symbol.species]() {
-    	return this;
-  	}
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-  	cloneCar() {
-    	const obj = this.constructor[Symbol.species];
+  get brand() {
+    return this._brand;
+  }
 
-    	return new obj();
-  	}
+  set brand(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Value must be a string');
+    }
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Value must be a string');
+    }
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Value must be a string');
+    }
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Obj = this.constructor[Symbol.species];
+    return new Obj();
+  }
 }
