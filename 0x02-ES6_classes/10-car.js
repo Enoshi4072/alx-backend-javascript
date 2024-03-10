dc8b6dd88691aeecb9e8e20f9c8d9ae806795805
Enoshi4072 <enockkibet6642@gmail.com>
2024-03-10 04:04:14 +0000

    10. Vroom

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,43 @@
+export default class Car {
+	constructor (brand, motor, color) {
+		this._brand = brand;
+		this._motor = motor;
+		this._color = color;
+	}
+	get brand() {
+		return this._brand;
+	}
+	set brand(value) {
+		if (typeof value !== 'string') {
+			throw new Error ('Value must be a string');
+		}
+		this._brand = value;
+	}
+	get motor() {
+                return this._motor;
+        }
+        set motor(value) {
+                if (typeof value !== 'string') {
+                        throw new Error ('Value must be a string');
+                }
+                this._motor = value;
+        }
+	get color() {
+                return this._color;
+        }
+        set color(value) {
+                if (typeof value !== 'string') {
+                        throw new Error ('Value must be a string');
+                }
+                this._color = value;
+        }
+	static get [Symbol.species]() {
+    	return this;
+  	}
+
+  	cloneCar() {
+    	const obj = this.constructor[Symbol.species];
+
+    	return new obj();
+  	}
+}
