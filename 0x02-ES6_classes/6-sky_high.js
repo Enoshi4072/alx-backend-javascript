04c3c0f4ba5d99d80e15a5fa3c4fcf7a2caa7eff
Enoshi4072 <enockkibet6642@gmail.com>
2024-03-10 07:39:36 +0000

    6. Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,19 +1,23 @@
-import Building from './5-building'
+import Building from './5-building';
+
 export default class SkyHighBuilding extends Building {
-	constructor (sqft, floors) {
-		super(sqft);
-		this._floors = floors;
-	}
-	get floors() {
-		return this._floors;
-	}
-	set floors(value) {
-		if (typeof value !== 'number') {
-			throw new TypeError ('Value must be a number');
-		}
-		this._floors = value;
-	}
-	evacuationWarningMessage() {
-		return `Evacuatr slowly the ${this.floors} floors`;
-	}
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Value must be a number');
+    }
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
