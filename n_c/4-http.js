ce772bed9dd3aa19a63ff3106aef6842cbb3e0ad
Enoshi4072 <enockkibet6642@gmail.com>
2024-06-18 20:56:42 +0000

    corr2

---
+++ n_c/4-http.js
@@ -0,0 +1,20 @@
+// Import the HTTP module
+const http = require('http');
+
+const port = 1245;
+const host = 'localhost';
+
+// Create a server
+const app = http.createServer((req, res) => {
+  // Set the response headers
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Write the response body
+  res.end('Hello Holberton School!');
+});
+
+// Set the server to listen on port 1245
+app.listen(port, host);
+
+// Export the server
+module.exports = app;
